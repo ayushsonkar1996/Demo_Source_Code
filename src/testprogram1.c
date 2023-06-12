23413469cd90c27cf796c21d88f10112b15c4197
ayushsonkar1996 <134266015+ayushsonkar1996@users.noreply.github.com>
2023-06-12 17:11:29 +0530

    Update  changes for testprogram1.c

--- src/testprogram1.c
+++ src/testprogram1.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
-
 int fun(int x, int y)
 {
-	
-	if (x/y == 0)
-	printf("testcase pass\n");
-	else 
-	printf("testcase fail\n");
-	
+        int val;
+        if(y == 0)
+        {
+                printf("division not possible, as number divided by 0 give you a infine value\n");
+        }
+        else
+        {
+                val = x/y;
+                printf("output value is %d\n",val);
+        }
 }
